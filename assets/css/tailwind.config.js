f4500152e3b38084217669f4154cb5f51cb7db98
thanh <hoc.hoc.hoc1996@gmail.com>
2020-11-28 21:18:39 +0700

    fix

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    display: ['responsive', 'group-hover'],
+    textDecoration: ['hover', 'group-hover'],
+    padding: ['responsive'],
+    inset: ['responsive']
+  },
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+}
